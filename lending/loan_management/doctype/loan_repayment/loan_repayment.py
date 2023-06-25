1d09ced68cebebc825549782e462012cb06e8748
Deepesh Garg <deepeshgarg6@gmail.com>
2023-06-25 22:09:49 +0530

    fix: Incorrect imports

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -7,8 +7,8 @@ from frappe import _
 from frappe.utils import add_days, cint, date_diff, flt, get_datetime, getdate
 
 import erpnext
-from lending.accounts.general_ledger import make_gl_entries
-from lending.controllers.accounts_controller import AccountsController
+from erpnext.accounts.general_ledger import make_gl_entries
+from erpnext.controllers.accounts_controller import AccountsController
 from lending.loan_management.doctype.loan.loan import update_all_linked_loan_customer_npa_status
 from lending.loan_management.doctype.loan_interest_accrual.loan_interest_accrual import (
 	get_last_accrual_date,
