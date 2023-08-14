24aaa95615b9fc901d6cfafe10fa21b4956751ab
Deepesh Garg <deepeshgarg6@gmail.com>
2023-08-14 17:12:39 +0530

    chore: linting issues

--- lending/patches/v15_0/update_loan_types.py
+++ lending/patches/v15_0/update_loan_types.py
@@ -3,12 +3,20 @@
 
 import frappe
 
+
 def execute():
-	for loan_type in frappe.db.get_all("Loan Type", fields=["name", "payment_account", "loan_account", "interest_income_account"]):
-		frappe.db.set_value("Loan Type", loan_type.name, {
-			"interest_receivable_account": loan_type.loan_account,
-			"charges_receivable_account": loan_type.loan_account,
-			"penalty_receivable_account": loan_type.loan_account,
-			"suspense_interest_receivable": loan_type.loan_account,
-			"suspense_collection_account": loan_type.payment_account
-		})
+	for loan_type in frappe.db.get_all(
+		"Loan Type", fields=["name", "payment_account", "loan_account", "interest_income_account"]
+	):
+		frappe.db.set_value(
+			"Loan Type",
+			loan_type.name,
+			{
+				"interest_receivable_account": loan_type.loan_account,
+				"charges_receivable_account": loan_type.loan_account,
+				"penalty_receivable_account": loan_type.loan_account,
+				"suspense_interest_receivable": loan_type.loan_account,
+				"suspense_interest_income": loan_type.interest_income_account,
+				"suspense_collection_account": loan_type.payment_account,
+			},
+		)
