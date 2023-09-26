0ee0377d30ad07adaa3ccbc23bfc1a10760d515f
anandbaburajan <anandbaburajan@gmail.com>
2023-09-26 14:48:57 +0530

    chore: remove custom fields on uninstall

--- lending/install.py
+++ lending/install.py
@@ -161,3 +161,33 @@ def make_property_setter_for_journal_entry():
 def after_install():
 	create_custom_fields(LOAN_CUSTOM_FIELDS, ignore_validate=True)
 	make_property_setter_for_journal_entry()
+
+
+def before_uninstall():
+	delete_custom_fields(LOAN_CUSTOM_FIELDS)
+
+
+def delete_custom_fields(custom_fields):
+	"""
+	:param custom_fields: a dict like `{'Customer': [{fieldname: 'test', ...}]}`
+	"""
+
+	for doctypes, fields in custom_fields.items():
+		if isinstance(fields, dict):
+			# only one field
+			fields = [fields]
+
+		if isinstance(doctypes, str):
+			# only one doctype
+			doctypes = (doctypes,)
+
+		for doctype in doctypes:
+			frappe.db.delete(
+				"Custom Field",
+				{
+					"fieldname": ("in", [field["fieldname"] for field in fields]),
+					"dt": doctype,
+				},
+			)
+
+			frappe.clear_cache(doctype=doctype)
