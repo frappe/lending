33baec062b4a4526d7084b7db587a038ac402cf0
Anand Baburajan <anandbaburajan@gmail.com>
2023-10-09 11:35:20 +0530

    feat: add loan product code, penalty_interest_method, broken_period_interest_charged, auto-closure fields (#80)
    * feat: add loan product code, penalty_interest_method, broken_period_interest_charged, auto-closure fields
    
    * test: fixing loan product tests

---
+++ lending/patches/v15_0/add_loan_product_code_and_rename_loan_name.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+import frappe
+from frappe.model.utils.rename_field import rename_field
+
+
+def execute():
+	try:
+		rename_field("Loan Product", "loan_name", "product_name")
+
+	except Exception as e:
+		if e.args[0] != 1054:
+			raise
+
+	for loan_product in frappe.db.get_all("Loan Product", fields=["name", "product_name"]):
+		frappe.db.set_value("Loan Product", loan_product.name, "product_code", loan_product.product_name)
