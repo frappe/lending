dfd28d562357d2398383d9ffbe15aa1c9051f2ed
Abhishek <abhishek.p@8848digital.com>
2023-11-20 16:11:27 +0530

    Loan API:Changed parameter name

--- lending/api.py
+++ lending/api.py
@@ -1,28 +1,28 @@
 import frappe
 
 @frappe.whitelist()
-def get_loan_list(hub=None,loan_act=None,cust_urn=None,cust_name=None):
+def get_loan_list(hub=None,loan_account_number=None,customer_urn=None,customer_name=None):
 	filters = {}
 	loan = ''
 	if hub != None:
 		filters["hub"] = hub
-	if loan_act != None:
-		filters["loan_account_number"] = loan_act
-	if cust_name:
-		cust_name = frappe.db.get_value('Customer',{'customer_name':cust_name},'customer_urn')
-	if not cust_urn  and cust_name:
-		filters["applicant"] = cust_name
-	if cust_urn  and not cust_name:
-		filters["applicant"] = cust_urn
-	if (cust_urn and cust_name) and (cust_urn  == cust_name):
-		filters["applicant"] = cust_urn
+	if loan_account_number != None:
+		filters["loan_account_number"] = loan_account_number
+	if customer_name:
+		customer_name = frappe.db.get_value('Customer',{'customer_name':customer_name},'customer_urn')
+	if not customer_urn  and customer_name:
+		filters["applicant"] = customer_name
+	if customer_urn  and not customer_name:
+		filters["applicant"] = customer_urn
+	if (customer_urn and customer_name) and (customer_urn  == customer_name):
+		filters["applicant"] = customer_urn
 	if filters != {}:
 		loan = frappe.db.get_list('Loan',filters = filters, pluck='name')
-	if (cust_urn and cust_name) and (cust_urn  != cust_name):
+	if (customer_urn and customer_name) and (customer_urn  != customer_name):
 		a = []
-		loan_urn = frappe.db.get_list('Loan',filters = {'applicant':cust_urn}, pluck='name')
+		loan_urn = frappe.db.get_list('Loan',filters = {'applicant':customer_urn}, pluck='name')
 		a.extend(loan_urn)
-		loan_name = frappe.db.get_list('Loan',filters = {'applicant':cust_name}, pluck='name')
+		loan_name = frappe.db.get_list('Loan',filters = {'applicant':customer_name}, pluck='name')
 		a.extend(loan_name)
 		if loan:
 			loan.extend(a)
