da3a1e5cd3b5c80cc3dc9ab084b93daed258dcd9
Deepesh Garg <deepeshgarg6@gmail.com>
2023-11-24 15:08:12 +0530

    fix: queries

---
+++ lending/loan_management/doctype/process_loan_interest_change/process_loan_interest_change.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class ProcessLoanInterestChange(Document):
+	def on_submit(self):
+		loan = frappe.qb.DocType("Loan")
+		if self.loan_product:
+			frappe.qb.update(loan).set(loan.rate_of_interest, self.new_rate_of_interest).where(
+				(loan.status.isin(["Disbursed", "Partially Disbursed", "Sanctioned"]))
+				& (loan.loan_product == self.loan_product)
+			).run()
+		elif self.get("loans"):
+			loans = [d.loan for d in self.get("loans")]
+			frappe.qb.update(loan).set(loan.rate_of_interest, self.new_rate_of_interest).where(
+				(loan.name.isin(loans))
+				& (loan.status.isin(["Disbursed", "Partially Disbursed", "Sanctioned"]))
+			).run()
