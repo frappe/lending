5cd2486415274c4e86b68502020cc979b04c452e
Deepesh Garg <deepeshgarg6@gmail.com>
2024-09-02 17:47:56 +0530

    fix: Income balance on write off

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -311,7 +311,7 @@ class LoanRepayment(AccountsController):
 					self.posting_date,
 					amount=demand.paid_amount,
 					loan_repayment=self.name,
-					waiver_account=waiver_account if not (self.is_npa and self.is_write_off_waiver) else None,
+					waiver_account=waiver_account if not self.is_npa else None,
 					posting_date=self.posting_date,
 				)
 
@@ -1056,9 +1056,7 @@ class LoanRepayment(AccountsController):
 
 			self.add_gl_entry(payment_account, against_account, self.total_interest_paid, gle_map)
 
-			if (
-				self.repayment_type == "Interest Waiver" and not self.is_npa and not self.is_write_off_waiver
-			):
+			if self.repayment_type == "Interest Waiver" and not self.is_npa:
 				self.add_gl_entry(
 					account_details.interest_income_account,
 					account_details.interest_waiver_account,
@@ -1079,7 +1077,7 @@ class LoanRepayment(AccountsController):
 
 			self.add_gl_entry(payment_account, against_account, total_penalty_paid, gle_map)
 
-			if self.repayment_type == "Penalty Waiver" and not self.is_npa and not self.is_write_off_waiver:
+			if self.repayment_type == "Penalty Waiver" and not self.is_npa:
 				self.add_gl_entry(
 					account_details.penalty_income_account,
 					account_details.penalty_waiver_account,
@@ -1098,7 +1096,7 @@ class LoanRepayment(AccountsController):
 
 			self.add_gl_entry(payment_account, against_account, additional_interest, gle_map)
 
-			if self.repayment_type == "Penalty Waiver" and not self.is_npa and not self.is_write_off_waiver:
+			if self.repayment_type == "Penalty Waiver" and not self.is_npa:
 				self.add_gl_entry(
 					account_details.additional_interest_income,
 					account_details.additional_interest_waiver,
