4a7d06370b041ab7c60f718dcd6b68d755411810
Deepesh Garg <deepeshgarg6@gmail.com>
2024-10-06 15:57:11 +0530

    fix: Multiple job queing for process classification

--- lending/loan_management/doctype/process_loan_classification/process_loan_classification.py
+++ lending/loan_management/doctype/process_loan_classification/process_loan_classification.py
@@ -8,8 +8,6 @@ from frappe.utils import getdate
 
 class ProcessLoanClassification(Document):
 	def on_submit(self):
-		from lending.loan_management.doctype.loan.loan import update_days_past_due_in_loans
-
 		filters = {
 			"docstatus": 1,
 			"status": ("in", ["Disbursed", "Partially Disbursed", "Active", "Written Off", "Settled"]),
@@ -23,30 +21,65 @@ class ProcessLoanClassification(Document):
 
 		open_loans = frappe.get_all("Loan", filters=filters, pluck="name")
 
-		for loan in open_loans:
-			try:
-				update_days_past_due_in_loans(
-					loan_name=loan,
+		if self.loan:
+			process_loan_classification_batch(
+				open_loans,
+				self.posting_date,
+				self.loan_product,
+				self.name,
+				self.payment_reference,
+				self.is_backdated,
+			)
+		else:
+			BATCH_SIZE = 5000
+			batch_list = list(get_batches(open_loans, BATCH_SIZE))
+			for batch in batch_list:
+				frappe.enqueue(
+					process_loan_classification_batch,
+					open_loans=batch,
 					posting_date=self.posting_date,
 					loan_product=self.loan_product,
-					process_loan_classification=self.name,
-					ignore_freeze=True if self.payment_reference else False,
+					classification_process=self.name,
+					payment_reference=self.payment_reference,
 					is_backdated=self.is_backdated,
+					queue="long",
 				)
 
-				if len(open_loans) > 1:
-					frappe.db.commit()
-			except Exception as e:
-				if len(open_loans) == 1:
-					raise e
-				else:
-					frappe.log_error(
-						title="Process Loan Classification Error",
-						message=frappe.get_traceback(),
-						reference_doctype="Loan",
-						reference_name=loan,
-					)
-					frappe.db.rollback()
+
+def process_loan_classification_batch(
+	open_loans, posting_date, loan_product, classification_process, payment_reference, is_backdated
+):
+	from lending.loan_management.doctype.loan.loan import update_days_past_due_in_loans
+
+	for loan in open_loans:
+		try:
+			update_days_past_due_in_loans(
+				loan_name=loan,
+				posting_date=posting_date,
+				loan_product=loan_product,
+				process_loan_classification=classification_process,
+				ignore_freeze=True if payment_reference else False,
+				is_backdated=is_backdated,
+			)
+
+			if len(open_loans) > 1:
+				frappe.db.commit()
+		except Exception as e:
+			if len(open_loans) == 1:
+				raise e
+			else:
+				frappe.log_error(
+					title="Process Loan Classification Error",
+					message=frappe.get_traceback(),
+					reference_doctype="Loan",
+					reference_name=loan,
+				)
+				frappe.db.rollback()
+
+
+def get_batches(open_loans, batch_size):
+	for i in range(0, len(open_loans), batch_size):
+		yield open_loans[i : i + batch_size]
 
 
 def create_process_loan_classification(
