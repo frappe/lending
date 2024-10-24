893bc0c643cbb93dacbac2d6cc4c7bfe228f5c2e
Deepesh Garg <deepeshgarg6@gmail.com>
2024-10-24 21:58:05 +0530

    fix: Auto close loan logic

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -759,7 +759,11 @@ class LoanRepayment(AccountsController):
 
 		shortfall_amount = self.pending_principal_amount - self.principal_amount_paid
 
-		if shortfall_amount > 0 and shortfall_amount <= auto_write_off_amount:
+		if (
+			shortfall_amount > 0
+			and shortfall_amount <= auto_write_off_amount
+			and self.payable_amount - self.amount_paid <= shortfall_amount
+		):
 			auto_close = True
 
 		excess_amount = self.principal_amount_paid - self.pending_principal_amount
