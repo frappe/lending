01dc4dd314f9dce1ccdf89b1fae228409cde7aa3
Ayush Chaudhari <ayush@frappe.io>
2025-02-13 15:31:02 +0530

    feat: repost test utils
    (cherry picked from commit 2adcda8d37d68a9d5eebf62f31cd73227fd7c058)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -512,6 +512,8 @@ class LoanRepayment(AccountsController):
 			process_loan_interest_accrual_for_loans,
 		)
 
+		if self.is_backdated:
+			return
 		self.flags.ignore_links = True
 		self.check_future_entries()
 		self.mark_as_unpaid()
