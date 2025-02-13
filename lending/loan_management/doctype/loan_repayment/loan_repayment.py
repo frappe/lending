7930ff90442451a763b8741cdacec32db72dbcfd
Ayush Chaudhari <ayush@frappe.io>
2025-02-13 15:31:02 +0530

    feat: repost test utils
    (cherry picked from commit e2e74b0377ef92aa35c9435dfe9aee8544c90390)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -100,8 +100,15 @@ class LoanRepayment(AccountsController):
 		)
 
 		if self.is_backdated:
-			frappe.enqueue()
-
+			if frappe.flags.in_test:
+				create_repost(self)
+			else:
+				frappe.enqueue(
+					create_repost,
+					repayment=self,
+					enqueue_after_commit=True,
+				)
+				return
 		reversed_accruals = []
 		make_sales_invoice_for_charge(
 			self.against_loan,
@@ -496,6 +503,8 @@ class LoanRepayment(AccountsController):
 			process_loan_interest_accrual_for_loans,
 		)
 
+		if self.is_backdated:
+			return
 		self.flags.ignore_links = True
 		# frappe.enqueue(self.cancel_linked_repayments, enqueue_after_commit=True)
 		if self.repayment_type == "Full Settlement":
@@ -654,6 +663,8 @@ class LoanRepayment(AccountsController):
 		if future_repayment_date:
 			self.is_backdated = True
 			# frappe.throw("Repayment already made till date {0}".format(get_datetime(future_repayment_date)))
+		else:
+			self.is_backdated = False
 
 	def validate_security_deposit_amount(self):
 		if self.repayment_type == "Security Deposit Adjustment":
