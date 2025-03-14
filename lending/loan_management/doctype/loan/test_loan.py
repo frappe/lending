af1e0a68cd661f1b73804d38b4e28c68c5287550
Deepesh Garg <deepeshgarg6@gmail.com>
2025-03-14 22:50:14 +0530

    feat: Bulk Repayment API
    (cherry picked from commit 7f33b21ced256da9c8dfa9c2d618db29d836e9fa)

--- lending/loan_management/doctype/loan/test_loan.py
+++ lending/loan_management/doctype/loan/test_loan.py
@@ -28,7 +28,10 @@ from lending.loan_management.doctype.loan_disbursement.loan_disbursement import 
 from lending.loan_management.doctype.loan_interest_accrual.loan_interest_accrual import (
 	days_in_year,
 )
-from lending.loan_management.doctype.loan_repayment.loan_repayment import calculate_amounts
+from lending.loan_management.doctype.loan_repayment.loan_repayment import (
+	calculate_amounts,
+	post_bulk_payments,
+)
 from lending.loan_management.doctype.loan_security_release.loan_security_release import (
 	get_pledged_security_qty,
 )
@@ -2204,3 +2207,57 @@ class TestLoan(IntegrationTestCase):
 			repayment_type="Pre Payment",
 		)
 		repayment.submit()
+
+	def test_bulk_payments(self):
+		loan = create_loan(
+			"_Test Customer 1",
+			"Term Loan Product 5",
+			2700000,
+			"Repay Over Number of Periods",
+			1,
+			posting_date="2024-10-30",
+			rate_of_interest=17.25,
+			applicant_type="Customer",
+			limit_applicable_start="2024-10-28",
+			limit_applicable_end="2025-10-28",
+		)
+		loan.submit()
+
+		disbursement = make_loan_disbursement_entry(
+			loan.name,
+			335533,
+			disbursement_date="2024-11-25",
+			repayment_start_date="2025-01-24",
+			repayment_frequency="One Time",
+		)
+		disbursement.submit()
+
+		process_loan_interest_accrual_for_loans(
+			posting_date="2025-01-23", loan=loan.name, company="_Test Company"
+		)
+
+		payments = [
+			{
+				"doctype": "Loan Repayment",
+				"against_loan": loan.name,
+				"posting_date": "2025-01-20 00:10:00",
+				"amount_paid": 100000,
+				"loan_disbursement": disbursement.name,
+			},
+			{
+				"doctype": "Loan Repayment",
+				"against_loan": loan.name,
+				"posting_date": "2025-01-21 00:10:00",
+				"amount_paid": 100000,
+				"loan_disbursement": disbursement.name,
+			},
+			{
+				"doctype": "Loan Repayment",
+				"against_loan": loan.name,
+				"posting_date": "2025-01-22 00:10:00",
+				"amount_paid": 100000,
+				"loan_disbursement": disbursement.name,
+			},
+		]
+
+		post_bulk_payments(payments)
