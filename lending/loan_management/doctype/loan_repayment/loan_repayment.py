193f89a520a6e088f6670fea23475c4f4d57b718
Ayush Chaudhari <ayush@frappe.io>
2025-03-19 17:51:47 +0530

    fix: throw validation only when moratorium end date present
    (cherry picked from commit 57c7d9955d580baa44d6d1dafece178d874985fa)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -2030,12 +2030,13 @@ class LoanRepayment(AccountsController):
 			moratorium_end_date = frappe.db.get_value(
 				"Loan Repayment Schedule", {"loan": self.against_loan, "docstatus": 1}, "moratorium_end_date"
 			)
-			if get_datetime(moratorium_end_date) >= get_datetime(self.posting_date):
-				frappe.throw(
-					_(
-						"Cannot make Advance or Pre Payments during moratorium period. (Moratorium End Date: {}, Posting Date: {})"
-					).format(moratorium_end_date, self.posting_date)
-				)
+			if moratorium_end_date:
+				if get_datetime(moratorium_end_date) >= get_datetime(self.posting_date):
+					frappe.throw(
+						_(
+							"Cannot make Advance or Pre Payments during moratorium period. (Moratorium End Date: {}, Posting Date: {})"
+						).format(moratorium_end_date, self.posting_date)
+					)
 
 
 def create_repayment_entry(
