8f3d1d947f0a0cb51878b084b86f07c8c1ac3777
Ayush Chaudhari <ayush@frappe.io>
2025-03-26 19:33:23 +0530

    patch: only change changed fields
    (cherry picked from commit ca7b44fe656c1ad38f26ef7e63a66614d350e34d)

--- lending/patches/v15_0/add_index_for_custom_fields_for_JE.py
+++ lending/patches/v15_0/add_index_for_custom_fields_for_JE.py
@@ -1,5 +1,26 @@
-from lending.install import after_install
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 
 def execute():
-	after_install()
+	create_custom_fields(
+		{
+			"Journal Entry": [
+				{
+					"fieldname": "loan_transfer",
+					"fieldtype": "Link",
+					"label": "Loan Transfer",
+					"insert_after": "naming_series",
+					"options": "Loan Transfer",
+					"search_index": 1,
+				},
+				{
+					"fieldname": "loan",
+					"fieldtype": "Link",
+					"label": "Loan",
+					"insert_after": "loan_transfer",
+					"options": "Loan",
+					"search_index": 1,
+				},
+			],
+		}
+	)
