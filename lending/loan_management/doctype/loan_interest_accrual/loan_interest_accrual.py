bad63ff70dd0675dcfffaac7e90c0c0d2b7eb3bb
Deepesh Garg <deepeshgarg6@gmail.com>
2025-03-27 12:46:50 +0530

    chore: Resolve conflicts

--- lending/loan_management/doctype/loan_interest_accrual/loan_interest_accrual.py
+++ lending/loan_management/doctype/loan_interest_accrual/loan_interest_accrual.py
@@ -25,48 +25,6 @@ from lending.utils import daterange
 
 
 class LoanInterestAccrual(AccountsController):
-<<<<<<< HEAD
-=======
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
-
-	from typing import TYPE_CHECKING
-
-	if TYPE_CHECKING:
-		from frappe.types import DF
-
-		accrual_date: DF.Date | None
-		accrual_type: DF.Literal[
-			"Regular", "Repayment", "Disbursement", "Credit Adjustment", "Debit Adjustment", "Refund"
-		]
-		additional_interest_amount: DF.Currency
-		additional_interest_suspense_entry: DF.Link | None
-		amended_from: DF.Link | None
-		applicant: DF.DynamicLink | None
-		applicant_type: DF.Literal["Employee", "Member", "Customer"]
-		base_amount: DF.Currency
-		company: DF.Link | None
-		cost_center: DF.Link | None
-		interest_amount: DF.Currency
-		interest_type: DF.Literal["Normal Interest", "Penal Interest"]
-		is_npa: DF.Check
-		is_term_loan: DF.Check
-		last_accrual_date: DF.Date | None
-		loan: DF.Link
-		loan_demand: DF.Link | None
-		loan_disbursement: DF.Link | None
-		loan_product: DF.Link | None
-		loan_repayment_schedule: DF.Link | None
-		loan_repayment_schedule_detail: DF.Data | None
-		normal_interest_journal_entry: DF.Link | None
-		posting_date: DF.Datetime | None
-		process_loan_interest_accrual: DF.Link | None
-		rate_of_interest: DF.Float
-		start_date: DF.Datetime | None
-		unmark_npa: DF.Check
-	# end: auto-generated types
-
->>>>>>> f1236a3 (fix: Misc NPA related issues)
 	def validate(self):
 		if not self.posting_date:
 			self.posting_date = nowdate()
