6125b1cf7865cdf750a052038aaaca2cd4e6e80b
Ayush Chaudhari <ayush@frappe.io>
2025-03-28 17:41:53 +0530

    fix: remove unnecessary field
    (cherry picked from commit df1066c72fe27ef840f9bb308d59b795c5db99d0)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -2406,7 +2406,6 @@ def process_amount_for_loan(
 			loan_disbursement=loan_disbursement,
 		)
 
-	amounts["interest_accrued"] = accrued_interest
 	amounts["total_charges_payable"] = charges
 	amounts["pending_principal_amount"] = flt(pending_principal_amount, precision)
 	amounts["payable_principal_amount"] = flt(payable_principal_amount, precision)
@@ -2582,7 +2581,6 @@ def init_amounts():
 		"pending_principal_amount": 0.0,
 		"payable_principal_amount": 0.0,
 		"payable_amount": 0.0,
-		"interest_accrued": 0.0,
 		"unaccrued_interest": 0.0,
 		"unbooked_interest": 0.0,
 		"unbooked_penalty": 0.0,
