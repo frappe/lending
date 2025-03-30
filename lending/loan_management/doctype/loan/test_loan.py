ec1577bc12b75014b889aa475163d6b2667e64d5
Deepesh Garg <deepeshgarg6@gmail.com>
2025-03-30 16:35:48 +0530

    fix: Add timestamp to payment
    (cherry picked from commit 5467c2d405c83196d331f1350d0fb43039d91619)

--- lending/loan_management/doctype/loan/test_loan.py
+++ lending/loan_management/doctype/loan/test_loan.py
@@ -2334,10 +2334,10 @@ class TestLoan(IntegrationTestCase):
 		)
 		disbursement.submit()
 
-		process_daily_loan_demands(posting_date="2024-12-29", loan=loan.name)
+		process_daily_loan_demands(posting_date="2024-12-29 00:00:00", loan=loan.name)
 
 		repayment_entry = create_repayment_entry(
-			loan.name, "2024-12-29", 401621, loan_disbursement=disbursement.name
+			loan.name, "2024-12-29 00:00:10", 401621, loan_disbursement=disbursement.name
 		)
 
 		repayment_entry.submit()
