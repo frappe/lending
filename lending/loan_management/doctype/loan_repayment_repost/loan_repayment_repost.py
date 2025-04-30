c5ee0c3a04ee0b7c6953ba82c365da417bd772b5
Ayush Chaudhari <ayush@frappe.io>
2025-04-30 15:14:58 +0530

    fix: use sets+don't redo previously recalculated demands

--- lending/loan_management/doctype/loan_repayment_repost/loan_repayment_repost.py
+++ lending/loan_management/doctype/loan_repayment_repost/loan_repayment_repost.py
@@ -266,7 +266,7 @@ class LoanRepaymentRepost(Document):
 
 			repayment_doc.allocate_amount_against_demands(amounts, on_submit=True)
 			while True:
-				new_allocations = {i.loan_demand for i in self.get("repayment_details")}
+				new_allocations = {i.loan_demand for i in repayment_doc.get("repayment_details")}
 				repayment_doc.allocate_amount_against_demands(amounts, on_submit=True)
 				if len(new_allocations.difference(self.loan_demands_to_be_corrected)):
 					self.loan_demands_to_be_corrected.update(
