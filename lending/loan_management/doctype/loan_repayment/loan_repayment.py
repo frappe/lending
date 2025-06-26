b8b70d66a746e5302f616c8eeb3325a12808fc9a
Aysha <ayshapp468@gmail.com>
2025-06-26 12:58:08 +0530

    fix: debit entry was being skipped in gl entry when process_payroll_accounting_entry_based_on_employee is enabled in loan repayment via payroll

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -2136,29 +2136,30 @@ class LoanRepayment(AccountsController):
 		payment_party_type = self.applicant_type
 		payment_party = self.applicant
 
-		if not (
+		if (
 			hasattr(self, "process_payroll_accounting_entry_based_on_employee")
-			and self.process_payroll_accounting_entry_based_on_employee
+			and not self.process_payroll_accounting_entry_based_on_employee
 		):
 			payment_party_type = ""
 			payment_party = ""
-			gl_entries.append(
-				self.get_gl_dict(
-					{
-						"account": account,
-						"against": against_account,
-						"debit": amount,
-						"debit_in_account_currency": amount,
-						"against_voucher_type": "Loan",
-						"against_voucher": self.against_loan,
-						"remarks": _(remarks),
-						"cost_center": self.cost_center,
-						"party": payment_party if not is_waiver_entry else "",
-						"party_type": payment_party_type if not is_waiver_entry else "",
-						"posting_date": getdate(self.posting_date),
-					}
-				)
+
+		gl_entries.append(
+			self.get_gl_dict(
+				{
+					"account": account,
+					"against": against_account,
+					"debit": amount,
+					"debit_in_account_currency": amount,
+					"against_voucher_type": "Loan",
+					"against_voucher": self.against_loan,
+					"remarks": _(remarks),
+					"cost_center": self.cost_center,
+					"party": payment_party if not is_waiver_entry else "",
+					"party_type": payment_party_type if not is_waiver_entry else "",
+					"posting_date": getdate(self.posting_date),
+				}
 			)
+		)
 		gl_entries.append(
 			self.get_gl_dict(
 				{
