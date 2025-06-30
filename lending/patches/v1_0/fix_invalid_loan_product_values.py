51a308e707599fc61307866e906d1ef285e5297a
Nihantra Patel <nihantra@frappe.io>
2025-06-30 16:25:23 +0530

    fix: clean invalid Loan Product values during v15 to develop/v1 migration
    (cherry picked from commit 7f973992de138c71e506c39156e64d98d0b8d297)

---
+++ lending/patches/v1_0/fix_invalid_loan_product_values.py
@@ -0,0 +1,30 @@
+import frappe
+
+
+def execute():
+	fields_to_fix = {
+		"cyclic_day_of_the_month": 0,
+		"grace_period_in_days": 0,
+		"rate_of_interest": 0.0,
+		"maximum_loan_amount": 0.0,
+		"is_term_loan": 0,
+		"penalty_interest_rate": 0.0,
+		"days_past_due_threshold_for_npa": 0,
+		"excess_amount_acceptance_limit": 0.0,
+		"disabled": 0,
+		"validate_normal_repayment": 0,
+		"same_as_regular_interest_accounts": 0,
+		"min_days_bw_disbursement_first_repayment": 0,
+		"write_off_amount": 0.0,
+	}
+
+	if not frappe.db.table_exists("Loan Product"):
+		return
+
+	for field, default in fields_to_fix.items():
+		if field not in frappe.db.get_table_columns("Loan Product"):
+			continue
+
+		condition = f"(`{field}` IS NULL OR `{field}` NOT REGEXP '^-?[0-9]+(\\.[0-9]+)?$')"
+		query = f"UPDATE `tabLoan Product` SET `{field}` = %s WHERE {condition}"
+		frappe.db.sql(query, (default,))
