5ba8e19f12760a66d0feafbcca7a0a4878de16b3
Deepesh Garg <deepeshgarg6@gmail.com>
2025-07-11 18:51:52 +0530

    fix: Advance payment for daily schedule
    (cherry picked from commit 903667fe608a5b0cc943d3800d3a5eecdc8df099)
    
    # Conflicts:
    #	lending/loan_management/doctype/loan_repayment/test_loan_repayment.py
    (cherry picked from commit 5564ccf0c3b230a3d0f2f575e77ac69691c367f3)

--- lending/loan_management/doctype/loan_repayment/test_loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/test_loan_repayment.py
@@ -834,3 +834,135 @@ class TestLoanRepayment(FrappeTestCase):
 		loan.load_from_db()
 
 		self.assertEqual(loan.total_principal_paid, 0)
+<<<<<<< HEAD
+=======
+
+	def test_pre_payment_with_partial_unbooked_interest(self):
+		set_loan_accrual_frequency("Daily")
+
+		loan = create_loan(
+			"_Test Customer 1",
+			"Term Loan Product 4",
+			1000000,
+			"Repay Over Number of Periods",
+			24,
+			"Customer",
+			repayment_start_date="2025-02-05",
+			posting_date="2025-01-06",
+			rate_of_interest=28,
+		)
+		loan.submit()
+
+		make_loan_disbursement_entry(
+			loan.name, loan.loan_amount, disbursement_date="2025-01-06", repayment_start_date="2025-02-05"
+		)
+
+		process_loan_interest_accrual_for_loans(
+			loan=loan.name, posting_date="2025-02-04", company="_Test Company"
+		)
+
+		process_daily_loan_demands(loan=loan.name, posting_date="2025-02-05")
+
+		create_repayment_entry(loan.name, "2025-02-05", 54889).submit()
+
+		process_loan_interest_accrual_for_loans(
+			loan=loan.name, posting_date="2025-03-04", company="_Test Company"
+		)
+
+		process_daily_loan_demands(loan=loan.name, posting_date="2025-03-05")
+
+		create_repayment_entry(loan.name, "2025-03-05", 54889).submit()
+
+		process_loan_interest_accrual_for_loans(
+			loan=loan.name, posting_date="2025-04-04", company="_Test Company"
+		)
+
+		process_daily_loan_demands(loan=loan.name, posting_date="2025-04-05")
+
+		create_repayment_entry(loan.name, "2025-04-05", 54889).submit()
+
+		process_loan_interest_accrual_for_loans(
+			loan=loan.name, posting_date="2025-05-04", company="_Test Company"
+		)
+
+		process_daily_loan_demands(loan=loan.name, posting_date="2025-05-05")
+
+		create_repayment_entry(loan.name, "2025-05-05", 54889).submit()
+
+		process_loan_interest_accrual_for_loans(
+			loan=loan.name, posting_date="2025-05-20", company="_Test Company"
+		)
+
+		create_repayment_entry(loan.name, "2025-05-21", 3327, repayment_type="Pre Payment").submit()
+
+		demand_amount = frappe.db.get_value(
+			"Loan Demand",
+			{"loan": loan.name, "docstatus": 1, "demand_subtype": "Interest", "demand_date": "2025-05-21"},
+			"paid_amount",
+		)
+
+		self.assertEqual(demand_amount, 3327)
+
+		repayment_schedule = frappe.db.get_value(
+			"Loan Repayment Schedule", {"loan": loan.name, "docstatus": 1, "status": "Active"}, "name"
+		)
+		principal_amount, interest_amount = frappe.db.get_value(
+			"Repayment Schedule",
+			{"parent": repayment_schedule, "idx": 5},
+			["principal_amount", "interest_amount"],
+		)
+
+		self.assertEqual(flt(principal_amount, 2), 37593.01)
+		self.assertEqual(flt(interest_amount, 2), 17295.99)
+
+	def test_advance_payment_with_daily_frequency(self):
+		set_loan_accrual_frequency("Daily")
+
+		loan = create_loan(
+			"_Test Customer 1",
+			"Term Loan Product 4",
+			200000,
+			"Repay Over Number of Periods",
+			10,
+			"Customer",
+			repayment_start_date="2025-07-02",
+			posting_date="2025-07-01",
+			rate_of_interest=17,
+			repayment_frequency="Daily",
+		)
+		loan.submit()
+
+		make_loan_disbursement_entry(
+			loan.name,
+			loan.loan_amount,
+			disbursement_date="2025-07-01",
+			repayment_start_date="2025-07-02",
+			repayment_frequency="Daily",
+		)
+
+		process_loan_interest_accrual_for_loans(
+			loan=loan.name, posting_date="2025-07-01", company="_Test Company"
+		)
+
+		process_daily_loan_demands(loan=loan.name, posting_date="2025-07-02")
+
+		create_repayment_entry(loan.name, "2025-07-02", 40104, repayment_type="Advance Payment").submit()
+
+		repayment_schedule = frappe.get_doc(
+			"Loan Repayment Schedule", {"loan": loan.name, "docstatus": 1, "status": "Active"}
+		)
+
+		self.assertEqual(
+			repayment_schedule.get("repayment_schedule")[1].payment_date, getdate("2025-07-03")
+		)
+
+		self.assertEqual(repayment_schedule.get("repayment_schedule")[1].demand_generated, 1)
+
+		create_repayment_entry(loan.name, "2025-07-02", 40104, repayment_type="Advance Payment").submit()
+
+		self.assertEqual(
+			repayment_schedule.get("repayment_schedule")[1].payment_date, getdate("2025-07-03")
+		)
+
+		self.assertEqual(repayment_schedule.get("repayment_schedule")[1].demand_generated, 1)
+>>>>>>> 903667fe (fix: Advance payment for daily schedule)
