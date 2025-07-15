15ecee5997831377ef91c08f5e7270e16f530b71
Deepesh Garg <deepeshgarg6@gmail.com>
2025-07-15 20:47:22 +0530

    fix: Security deposit adjustment excess gl entry
    (cherry picked from commit d5297c965e8da0fe4662b426251a1a1f822b587b)

--- lending/loan_management/doctype/loan_repayment/test_loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/test_loan_repayment.py
@@ -885,3 +885,91 @@ class TestLoanRepayment(FrappeTestCase):
 		)
 
 		self.assertEqual(repayment_schedule.get("repayment_schedule")[1].demand_generated, 1)
+
+	def test_excess_payment_via_security_adjustment(self):
+		loan = create_loan(
+			"_Test Customer 1",
+			"Term Loan Product 4",
+			500000,
+			"Repay Over Number of Periods",
+			12,
+			"Customer",
+			posting_date="2024-03-25",
+			rate_of_interest=12,
+		)
+		loan.submit()
+
+		make_loan_disbursement_entry(
+			loan.name,
+			loan.loan_amount,
+			disbursement_date="2024-03-25",
+			repayment_start_date="2024-04-01",
+			withhold_security_deposit=1,
+		)
+
+		process_daily_loan_demands(posting_date="2024-09-01", loan=loan.name)
+
+		amounts = calculate_amounts(against_loan=loan.name, posting_date="2024-09-01")
+		payable_amount = round(float(amounts["payable_amount"] or 0.0), 2)
+
+		repayment_entry = create_repayment_entry(loan.name, "2024-09-01", payable_amount)
+		repayment_entry.submit()
+
+		process_loan_interest_accrual_for_loans(
+			posting_date="2024-10-05", loan=loan.name, company="_Test Company"
+		)
+
+		loan.load_from_db()
+		loan.freeze_account = 1
+		loan.freeze_date = "2024-09-03"
+		loan.save()
+
+		amounts = calculate_amounts(against_loan=loan.name, posting_date="2024-09-05")
+		total_net_payable = (
+			round(
+				float(amounts["unaccrued_interest"] or 0.0)
+				+ float(amounts["interest_amount"] or 0.0)
+				+ float(amounts["penalty_amount"] or 0.0)
+				+ float(amounts["total_charges_payable"] or 0.0)
+				- float(amounts["available_security_deposit"] or 0.0)
+				+ float(amounts["unbooked_interest"] or 0.0)
+				+ float(amounts["unbooked_penalty"] or 0.0)
+				+ float(amounts["pending_principal_amount"] or 0.0),
+				2,
+			)
+			+ 20
+		)
+
+		loan_adjustment = frappe.get_doc(
+			{
+				"doctype": "Loan Adjustment",
+				"loan": loan.name,
+				"posting_date": "2024-09-05",
+				"foreclosure_type": "Internal Foreclosure",
+				"adjustments": [{"loan_repayment_type": "Normal Repayment", "amount": total_net_payable}],
+			}
+		)
+		loan_adjustment.submit()
+
+		# Since excess amount is more than 0 it should be parked in customer refund account
+		customer_refund_account = frappe.get_value(
+			"Loan Product", loan.loan_product, "customer_refund_account"
+		)
+
+		loan_repayment = frappe.db.get_value(
+			"Loan Repayment",
+			{"repayment_type": "Security Deposit Adjustment", "against_loan": loan.name, "docstatus": 1},
+			"name",
+		)
+
+		gl_entry = frappe.db.get_value(
+			"GL Entry",
+			{
+				"voucher_no": loan_repayment,
+				"voucher_type": "Loan Repayment",
+				"account": customer_refund_account,
+			},
+			"name",
+		)
+
+		self.assertTrue(gl_entry, "GL Entry not created for customer refund account")
