8e62d1a18822519cc6881d08f2cbbceff60ca661
Deepesh Garg <deepeshgarg6@gmail.com>
2025-07-28 09:18:06 +0530

    fix: first two day accrual post disbursement
    (cherry picked from commit 1629e3a02543d3d4178465411eb39448b246c705)

--- lending/loan_management/doctype/loan_interest_accrual/loan_interest_accrual.py
+++ lending/loan_management/doctype/loan_interest_accrual/loan_interest_accrual.py
@@ -426,8 +426,7 @@ def process_loan_interest_accrual_per_schedule(
 
 	for parent in parent_wise_schedules:
 		for payment_date in parent_wise_schedules[parent]:
-			last_accrual_date_for_schedule = add_days(last_accrual_date_map.get(parent), 1)
-
+			last_accrual_date_for_schedule = last_accrual_date_map.get(parent)
 			pending_principal_amount = get_principal_amount_for_term_loan(parent, payment_date)
 			payable_interest = get_interest_for_term(
 				loan.company,
@@ -454,7 +453,7 @@ def process_loan_interest_accrual_per_schedule(
 						accrual_date=payment_date,
 					)
 
-				last_accrual_date_map[parent] = payment_date
+				last_accrual_date_map[parent] = add_days(payment_date, 1)
 
 	return total_payable_interest
 
@@ -904,7 +903,7 @@ def get_last_accrual_date(
 			if dates.moratorium_type == "EMI" and dates.moratorium_end_date:
 				final_date = dates.moratorium_end_date
 			else:
-				final_date = add_days(dates.posting_date, -1)
+				final_date = dates.posting_date
 
 			return final_date
 
@@ -1126,6 +1125,7 @@ def get_parent_wise_dates(loan, posting_date, loan_disbursement=None):
 	schedule_dates = []
 
 	accrual_schedule_map = {}
+	parent_wise_schedules = frappe._dict()
 
 	freeze_date = frappe.db.get_value("Loan", loan, "freeze_date")
 	if freeze_date and getdate(freeze_date) < getdate(posting_date):
@@ -1140,7 +1140,9 @@ def get_parent_wise_dates(loan, posting_date, loan_disbursement=None):
 			loan_disbursement=loan_disbursement,
 		)
 
+		parent_wise_schedules.setdefault(schedule, [])
 		accrual_schedule_map[schedule] = last_accrual_date
+		parent_wise_schedules[schedule].append(getdate(last_accrual_date))
 
 		schedule_filters = {
 			"parent": schedule,
@@ -1157,7 +1159,6 @@ def get_parent_wise_dates(loan, posting_date, loan_disbursement=None):
 			or []
 		)
 
-	parent_wise_schedules = frappe._dict()
 	for schedule_date in schedule_dates:
 		parent_wise_schedules.setdefault(schedule_date.parent, [])
 		accrual_date = add_days(schedule_date.payment_date, -1)
