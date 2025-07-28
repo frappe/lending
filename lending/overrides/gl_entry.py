47d4f6a13c8172414a6c43b594f24c565a8bbab9
Deepesh Garg <deepeshgarg6@gmail.com>
2025-07-28 12:41:38 +0530

    fix: Value date updation for loan demand

--- lending/overrides/gl_entry.py
+++ lending/overrides/gl_entry.py
@@ -1,7 +1,7 @@
 def update_value_date_in_gl_dict(doc, gl_dict):
 	if doc.get("value_date"):
 		gl_dict["value_date"] = doc.value_date
-	elif doc.get("disbursement_date"):
+	elif doc.doctype == "Loan Disbursement" and doc.get("disbursement_date"):
 		gl_dict["value_date"] = doc.disbursement_date
 	elif doc.get("accrual_date"):
 		gl_dict["value_date"] = doc.accrual_date
