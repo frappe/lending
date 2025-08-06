a067e451bba2a9a73b7338e86a56b524fbb12faa
Ayush Chaudhari <ayush@frappe.io>
2025-08-06 18:15:30 +0530

    fix: old test boilerplate

--- lending/loan_management/doctype/bulk_repayment_log/test_bulk_repayment_log.py
+++ lending/loan_management/doctype/bulk_repayment_log/test_bulk_repayment_log.py
@@ -2,28 +2,8 @@
 # See license.txt
 
 # import frappe
-from frappe.tests import IntegrationTestCase, UnitTestCase
-
-# On IntegrationTestCase, the doctype test records and all
-# link-field test record dependencies are recursively loaded
-# Use these module variables to add/remove to/from that list
-EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+import unittest
 
 
-class UnitTestBulkRepaymentLog(UnitTestCase):
-	"""
-	Unit tests for BulkRepaymentLog.
-	Use this class for testing individual functions and methods.
-	"""
-
-	pass
-
-
-class IntegrationTestBulkRepaymentLog(IntegrationTestCase):
-	"""
-	Integration tests for BulkRepaymentLog.
-	Use this class for testing interactions between multiple components.
-	"""
-
+class TestBulkRepaymentLog(unittest.TestCase):
 	pass
