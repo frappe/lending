264e3ba2083dee09a68dc0081304b98c58af75f3
Deepesh Garg <deepeshgarg6@gmail.com>
2025-08-07 22:45:32 +0530

    fix: Auto closure loan scenario
    (cherry picked from commit 598880213881d95e4492ac4c9aa29845958e0f66)
    
    # Conflicts:
    #	lending/loan_management/doctype/loan_repayment/loan_repayment.py
    #	lending/loan_management/doctype/loan_repayment/test_loan_repayment.py

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -357,8 +357,7 @@ class LoanRepayment(AccountsController):
 				loan=self.against_loan,
 			)
 
-		if getattr(self.flags, "auto_waiver_needed", False):
-			self.create_auto_waiver()
+		self.create_auto_waiver()
 
 	def create_repost(self):
 		repost = frappe.new_doc("Loan Repayment Repost")
@@ -978,7 +977,7 @@ class LoanRepayment(AccountsController):
 			auto_write_off_amount = flt(
 				frappe.db.get_value("Loan Product", self.loan_product, "write_off_amount")
 			)
-			if self.amount_paid >= self.payable_amount - auto_write_off_amount and self.auto_close_loan():
+			if self.amount_paid >= self.payable_amount - auto_write_off_amount and self.flags.auto_close:
 				if self.repayment_schedule_type != "Line of Credit":
 					query = query.set(loan.status, "Closed")
 					query = query.set(loan.closure_date, self.posting_date)
@@ -991,7 +990,7 @@ class LoanRepayment(AccountsController):
 				self.update_repayment_schedule_status()
 
 		elif (
-			self.auto_close_loan()
+			self.flags.auto_close
 			and self.repayment_type
 			in (
 				"Normal Repayment",
@@ -1039,7 +1038,7 @@ class LoanRepayment(AccountsController):
 			d.paid_amount for d in self.get("repayment_details") if d.demand_subtype == "Principal"
 		)
 		if (
-			self.auto_close_loan()
+			self.flags.auto_close
 			and overdue_principal_paid > 0
 			and overdue_principal_paid < self.payable_principal_amount
 			and self.principal_amount_paid - overdue_principal_paid > 0
@@ -1161,7 +1160,7 @@ class LoanRepayment(AccountsController):
 			frappe.db.set_value("Loan Repayment Schedule", repayment_schedule, "status", status)
 
 	def auto_close_loan(self):
-		auto_close = False
+		self.flags.auto_close = False
 
 		precision = cint(frappe.db.get_default("currency_precision")) or 2
 
@@ -1191,7 +1190,10 @@ class LoanRepayment(AccountsController):
 			total_payable = self.payable_amount
 
 		if self.excess_amount > 0 and self.repayment_schedule_type == "Line of Credit":
-			auto_close = True
+			self.flags.auto_close = True
+
+		shortfall_amount = flt(self.pending_principal_amount - self.principal_amount_paid, precision)
+		shortfall_amount += flt(self.total_charges_payable - self.total_charges_paid, precision)
 
 		if (
 			auto_write_off_amount
@@ -1199,15 +1201,11 @@ class LoanRepayment(AccountsController):
 			and shortfall_amount <= auto_write_off_amount
 			and flt(total_payable - self.amount_paid, precision) <= flt(shortfall_amount, precision)
 		):
-			auto_close = True
-			self.set_excess_amount_for_waiver(total_payable)
-			self.enable_auto_waiver_if_normal_repayment()
+			self.flags.auto_close = True
 
 		excess_amount = self.principal_amount_paid - self.pending_principal_amount
 		if excess_amount > 0 and excess_amount <= excess_amount_limit:
-			auto_close = True
-			self.set_excess_amount_for_waiver(total_payable)
-			self.enable_auto_waiver_if_normal_repayment()
+			self.flags.auto_close = True
 
 		if (
 			self.principal_amount_paid >= self.pending_principal_amount
@@ -1215,10 +1213,9 @@ class LoanRepayment(AccountsController):
 			and flt(self.excess_amount) <= flt(excess_amount_limit)
 			and flt(total_payable - self.amount_paid, precision) <= flt(auto_write_off_amount, precision)
 		):
-			auto_close = True
-			self.set_excess_amount_for_waiver(total_payable)
-			self.enable_auto_waiver_if_normal_repayment()
+			self.flags.auto_close = True
 
+<<<<<<< HEAD
 		return auto_close
 
 	def set_excess_amount_for_waiver(self, total_payable):
@@ -1234,39 +1231,47 @@ class LoanRepayment(AccountsController):
 		amounts = calculate_amounts(
 			against_loan=against_loan, posting_date=posting_date, loan_disbursement=loan_disbursement
 		)
+=======
+		return self.flags.auto_close
+>>>>>>> 59888021 (fix: Auto closure loan scenario)
 
+	def get_auto_waiver_type(self, amounts):
 		precision = cint(frappe.db.get_default("currency_precision")) or 2
 
 		if flt(amounts.get("interest_amount", 0), precision) > 0:
-			self.flags.waiver_type = "Interest Waiver"
+			waiver_type = "Interest Waiver"
 		if flt(amounts.get("penalty_amount", 0), precision) > 0:
-			self.flags.waiver_type = "Penalty Waiver"
+			waiver_type = "Penalty Waiver"
 		if flt(amounts.get("total_charges_payable", 0), precision) > 0:
-			self.flags.waiver_type = "Charges Waiver"
+			waiver_type = "Charges Waiver"
 		if flt(amounts.get("pending_principal_amount", 0), precision) > 0:
-			self.flags.waiver_type = "Principal Adjustment"
+			waiver_type = "Principal Adjustment"
 
-		return (
-			flt(amounts.get("interest_amount", 0), precision),
-			flt(amounts.get("penalty_amount", 0), precision),
-			flt(amounts.get("total_charges_payable", 0), precision),
-			flt(amounts.get("pending_principal_amount", 0), precision),
-		)
+		return waiver_type
 
 	def create_auto_waiver(self):
-		waiver_type = getattr(self.flags, "waiver_type", None)
-		if not waiver_type:
-			return
+		if self.repayment_type == "Normal Repayment" and self.flags.auto_close:
+			amounts = calculate_amounts(
+				against_loan=self.against_loan,
+				posting_date=self.value_date,
+				loan_disbursement=self.loan_disbursement,
+				payment_type=self.repayment_type,
+			)
 
+<<<<<<< HEAD
 		amounts = calculate_amounts(
 			against_loan=self.against_loan,
 			posting_date=self.posting_date,
 			loan_disbursement=self.loan_disbursement,
 			payment_type=self.repayment_type,
 		)
+=======
+			waiver_type = self.get_auto_waiver_type(amounts)
+>>>>>>> 59888021 (fix: Auto closure loan scenario)
 
-		precision = cint(frappe.db.get_default("currency_precision")) or 2
+			precision = cint(frappe.db.get_default("currency_precision")) or 2
 
+<<<<<<< HEAD
 		# Map waiver type to correct key
 		key_map = {
 			"Interest Waiver": "interest_amount",
@@ -1276,17 +1281,27 @@ class LoanRepayment(AccountsController):
 		}
 		amount_key = key_map.get(waiver_type)
 		waiver_amount = flt(amounts.get(amount_key, 0), precision)
+=======
+			key_map = {
+				"Interest Waiver": "interest_amount",
+				"Penalty Waiver": "penalty_amount",
+				"Charges Waiver": "total_charges_payable",
+				"Principal Adjustment": "pending_principal_amount",
+			}
+			amount_key = key_map.get(waiver_type)
+			waiver_amount = flt(amounts.get(amount_key, 0), precision)
+>>>>>>> 59888021 (fix: Auto closure loan scenario)
 
-		if waiver_amount <= 0:
-			return
+			if waiver_amount <= 0:
+				return
 
-		from lending.loan_management.doctype.loan_restructure.loan_restructure import (
-			create_loan_repayment,
-		)
+			from lending.loan_management.doctype.loan_restructure.loan_restructure import (
+				create_loan_repayment,
+			)
 
-		create_loan_repayment(
-			self.against_loan, self.posting_date, waiver_type, waiver_amount, is_write_off_waiver=1
-		)
+			create_loan_repayment(
+				self.against_loan, self.posting_date, waiver_type, waiver_amount, is_write_off_waiver=1
+			)
 
 	def mark_as_unpaid(self):
 		if self.repayment_type in (
@@ -2003,7 +2018,7 @@ class LoanRepayment(AccountsController):
 				)
 
 		if flt(self.excess_amount, precision):
-			if self.auto_close_loan() and self.excess_amount < 0:
+			if self.flags.auto_close and self.excess_amount < 0:
 				against_account = account_details.interest_waiver_account
 				if not against_account:
 					frappe.throw(_("Interest Waiver Account is mandatory"))
