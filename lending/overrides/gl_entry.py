acd0e223b31530c5a21d3965050aeb3f588ffae3
Deepesh Garg <deepeshgarg6@gmail.com>
2025-08-12 13:11:47 +0530

    chore: Accrual GL value date update

--- lending/overrides/gl_entry.py
+++ lending/overrides/gl_entry.py
@@ -3,7 +3,7 @@ def update_value_date_in_gl_dict(doc, gl_dict):
 		gl_dict["value_date"] = doc.value_date
 	elif doc.doctype == "Loan Disbursement" and doc.get("disbursement_date"):
 		gl_dict["value_date"] = doc.disbursement_date
-	elif doc.get("accrual_date"):
-		gl_dict["value_date"] = doc.accrual_date
+	elif doc.doctype == "Loan Interest Accrual":
+		gl_dict["value_date"] = doc.posting_date
 	elif doc.get("demand_date"):
 		gl_dict["value_date"] = doc.demand_date
