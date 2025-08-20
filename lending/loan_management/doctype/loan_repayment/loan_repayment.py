55962c2e2c11220cbd897cec5984ce1af37fb3f9
Ayush Chaudhari <ayush@frappe.io>
2025-08-20 17:33:14 +0530

    perf: use order by desc instead of max and put it in a subquery

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -6,6 +6,7 @@ import traceback
 
 import frappe
 from frappe import _
+from frappe.query_builder import Order
 from frappe.query_builder.functions import Coalesce, Max, Round, Sum
 from frappe.utils import add_days, cint, flt, get_datetime, getdate, random_string
 
@@ -2983,6 +2984,7 @@ def get_accrued_interest(
 
 
 def get_unbooked_interest_for_bulk_loans(loans, posting_date):
+
 	loan_demand_doc = frappe.qb.DocType("Loan Demand")
 	query1 = (
 		frappe.qb.from_(loan_demand_doc)
@@ -2990,19 +2992,25 @@ def get_unbooked_interest_for_bulk_loans(loans, posting_date):
 		.where(loan_demand_doc.demand_subtype == "Interest")
 		.where(loan_demand_doc.demand_date <= posting_date)
 		.where(loan_demand_doc.loan.isin(loans))
-		.groupby(loan_demand_doc.loan)
+		.orderby(loan_demand_doc.demand_date, order=Order.desc)
 		.select(
 			loan_demand_doc.loan.as_("loan"), Max(loan_demand_doc.demand_date).as_("last_demand_date")
 		)
 	)
 
-	loan_interest_accrual_doc = frappe.qb.DocType("Loan Interest Accrual")
 	query2 = (
+		frappe.qb.from_(query1)
+		.where(query1.loan.isin(loans))
+		.select(query1.loan, query1.last_demand_date)
+	)
+
+	loan_interest_accrual_doc = frappe.qb.DocType("Loan Interest Accrual")
+	query3 = (
 		frappe.qb.from_(loan_interest_accrual_doc)
-		.join(query1)
+		.join(query2)
 		.on(
-			(loan_interest_accrual_doc.loan == query1.loan)
-			& (loan_interest_accrual_doc.posting_date >= query1.last_demand_date)
+			(loan_interest_accrual_doc.loan == query2.loan)
+			& (loan_interest_accrual_doc.posting_date >= query2.last_demand_date)
 		)
 		.where(loan_interest_accrual_doc.interest_type == "Normal Interest")
 		.where(loan_interest_accrual_doc.docstatus == 1)
