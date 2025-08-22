ba965fb86dfd67df4c32da410197bbe17039ba03
Ayush Chaudhari <ayush@frappe.io>
2025-08-22 10:46:24 +0530

    style: better variable naming
    (cherry picked from commit b1900b66a8e4316da465e8035249513f6b48e99b)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -3044,7 +3044,7 @@ def get_accrued_interest(
 def get_unbooked_interest_for_bulk_loans(loans, posting_date):
 
 	loan_demand_doc = frappe.qb.DocType("Loan Demand")
-	query1 = (
+	query_get_last_demand_date_per_loan = (
 		frappe.qb.from_(loan_demand_doc)
 		.where(loan_demand_doc.docstatus == 1)
 		.where(loan_demand_doc.demand_subtype == "Interest")
@@ -3054,19 +3054,24 @@ def get_unbooked_interest_for_bulk_loans(loans, posting_date):
 		.select(loan_demand_doc.loan.as_("loan"), loan_demand_doc.demand_date.as_("last_demand_date"))
 	)
 
-	query2 = (
-		frappe.qb.from_(query1)
-		.where(query1.loan.isin(loans))
-		.select(query1.loan, query1.last_demand_date)
+	query_get_last_demand_date_for_all_loans = (
+		frappe.qb.from_(query_get_last_demand_date_per_loan)
+		.where(query_get_last_demand_date_per_loan.loan.isin(loans))
+		.select(
+			query_get_last_demand_date_per_loan.loan, query_get_last_demand_date_per_loan.last_demand_date
+		)
 	)
 
 	loan_interest_accrual_doc = frappe.qb.DocType("Loan Interest Accrual")
-	query3 = (
+	query_get_interest_amounts_for_all_loans = (
 		frappe.qb.from_(loan_interest_accrual_doc)
-		.join(query2)
+		.join(query_get_last_demand_date_for_all_loans)
 		.on(
-			(loan_interest_accrual_doc.loan == query2.loan)
-			& (loan_interest_accrual_doc.posting_date >= query2.last_demand_date)
+			(loan_interest_accrual_doc.loan == query_get_last_demand_date_for_all_loans.loan)
+			& (
+				loan_interest_accrual_doc.posting_date
+				>= query_get_last_demand_date_for_all_loans.last_demand_date
+			)
 		)
 		.where(loan_interest_accrual_doc.interest_type == "Normal Interest")
 		.where(loan_interest_accrual_doc.docstatus == 1)
@@ -3077,7 +3082,7 @@ def get_unbooked_interest_for_bulk_loans(loans, posting_date):
 			Sum(loan_interest_accrual_doc.interest_amount).as_("interest_amount"),
 		)
 	)
-	result = query3.run(as_dict=True)
+	result = query_get_interest_amounts_for_all_loans.run(as_dict=True)
 	return {i.loan: i.interest_amount for i in result}
 
 
