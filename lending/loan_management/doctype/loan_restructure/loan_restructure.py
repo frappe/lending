028af86b237fba5bd3ef745b1850a81979d22975
Nihantra Patel <nihantra@frappe.io>
2026-01-27 12:57:34 +0530

    fix: clear overdue principal demands during normal restructure
    (cherry picked from commit 0a7427ae9199f74664ce93e63f8d2ce8a13addce)
    
    # Conflicts:
    #	lending/loan_management/doctype/loan_restructure/loan_restructure.py
    #	lending/loan_management/doctype/loan_restructure/test_loan_restructure.py

--- lending/loan_management/doctype/loan_restructure/loan_restructure.py
+++ lending/loan_management/doctype/loan_restructure/loan_restructure.py
@@ -267,7 +267,12 @@ class LoanRestructure(AccountsController):
 			if self.unaccrued_interest and self.restructure_type == "Normal Restructure":
 				make_accrual_interest_entry_for_loans(posting_date=self.restructure_date, loan=self.loan)
 
+<<<<<<< HEAD
 				# self.make_waiver_and_capitalization_for_penalty()
+=======
+				self.make_waiver_and_capitalization_for_penalty()
+				self.set_principal_adjustment_on_restructure()
+>>>>>>> 0a7427ae (fix: clear overdue principal demands during normal restructure)
 				self.make_loan_repayment_for_adjustment()
 				self.make_loan_repayment_for_waiver()
 				# self.make_loan_adjustment_for_capitalization()
@@ -585,6 +590,10 @@ class LoanRestructure(AccountsController):
 				self.balance_penalty_amount,
 				restructure_name=self.name,
 			)
+
+	def set_principal_adjustment_on_restructure(self):
+		if flt(self.principal_overdue) > 0 and flt(self.principal_adjusted) == 0:
+			self.principal_adjusted = flt(self.principal_overdue)
 
 	def make_loan_repayment_for_adjustment(self):
 		if self.principal_adjusted:
