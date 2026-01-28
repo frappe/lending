b74d1efec436fa5aec0aff22b26e0d49ff2e075b
Nihantra Patel <nihantra@frappe.io>
2026-01-28 14:16:36 +0530

    fix: skip gl on Principal Adjustment and Restructure
    (cherry picked from commit bcd51b7e8d8bad6380c2b72cfff88ed1449ded79)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -1893,15 +1893,8 @@ class LoanRepayment(AccountsController):
 				)
 			return
 
-		if (
-			self.repayment_type == "Principal Adjustment"
-			and self.loan_restructure
-			and frappe.db.get_value(
-				"Loan Restructure", self.loan_restructure, "restructure_type"
-			) == "Normal Restructure"
-		):
+		if self.repayment_type == "Principal Adjustment" and self.loan_restructure:
 			return
-
 
 		if cancel:
 			make_reverse_gl_entries(voucher_type="Loan Repayment", voucher_no=self.name)
