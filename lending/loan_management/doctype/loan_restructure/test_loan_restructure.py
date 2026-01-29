c049b87664aae749cfabcaf7a4cdc8bd9648ce81
Nihantra Patel <nihantra@frappe.io>
2026-01-29 15:08:39 +0530

    fix: correct GL entries for unaccrued interest capitalization
    (cherry picked from commit 7bf59d6fcbae2afb9757419dfca10ddf841474b2)
    
    # Conflicts:
    #	lending/loan_management/doctype/loan_restructure/test_loan_restructure.py

--- lending/loan_management/doctype/loan_restructure/test_loan_restructure.py
+++ lending/loan_management/doctype/loan_restructure/test_loan_restructure.py
@@ -1,9 +1,219 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
 
+<<<<<<< HEAD
 # import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestLoanRestructure(FrappeTestCase):
 	pass
+=======
+import frappe
+from frappe.tests import IntegrationTestCase
+from frappe.utils import flt
+
+from lending.loan_management.doctype.process_loan_demand.process_loan_demand import (
+	process_daily_loan_demands,
+)
+from lending.loan_management.doctype.process_loan_interest_accrual.process_loan_interest_accrual import (
+	process_loan_interest_accrual_for_loans,
+)
+from lending.tests.test_utils import (
+	create_loan,
+	init_customers,
+	init_loan_products,
+	make_loan_disbursement_entry,
+	master_init,
+	set_loan_accrual_frequency,
+)
+
+
+class TestLoanRestructure(IntegrationTestCase):
+	def setUp(self):
+		master_init()
+		init_loan_products()
+		init_customers()
+		self.applicant2 = frappe.db.get_value("Customer", {"name": "_Test Loan Customer"}, "name")
+
+	def test_loan_restructure_capitalization(self):
+		set_loan_accrual_frequency(loan_accrual_frequency="Daily")
+
+		loan = create_loan(
+			"_Test Customer 1",
+			"Term Loan Product 4",
+			100000,
+			"Repay Over Number of Periods",
+			22,
+			repayment_start_date="2024-04-05",
+			posting_date="2024-02-20",
+			rate_of_interest=8.5,
+			applicant_type="Customer",
+			penalty_charges_rate=36,
+		)
+
+		loan.submit()
+
+		make_loan_disbursement_entry(
+			loan.name, loan.loan_amount, disbursement_date="2024-02-20", repayment_start_date="2024-04-05"
+		)
+
+		process_loan_interest_accrual_for_loans(
+			posting_date="2024-04-04", loan=loan.name, company="_Test Company"
+		)
+
+		process_daily_loan_demands(loan=loan.name, posting_date="2024-04-05")
+
+		process_loan_interest_accrual_for_loans(loan=loan.name, posting_date="2024-04-10")
+
+		sales_invoice = frappe.get_doc(
+			{
+				"doctype": "Sales Invoice",
+				"customer": "_Test Customer 1",
+				"company": "_Test Company",
+				"loan": loan.name,
+				"posting_date": "2024-02-20",
+				"value_date": "2024-02-20",
+				"posting_time": "00:06:10",
+				"set_posting_time": 1,
+				"items": [{"item_code": "Processing Fee", "qty": 1, "rate": 5000}],
+				"debit_to": "Processing Fee Receivable Account - _TC",
+			}
+		)
+		sales_invoice.submit()
+
+		sales_invoice = frappe.get_doc(
+			{
+				"doctype": "Sales Invoice",
+				"customer": "_Test Customer 1",
+				"company": "_Test Company",
+				"loan": loan.name,
+				"posting_date": "2024-02-20",
+				"value_date": "2024-02-20",
+				"posting_time": "00:06:10",
+				"set_posting_time": 1,
+				"items": [{"item_code": "Documentation Charge", "qty": 1, "rate": 1000}],
+			}
+		)
+		sales_invoice.submit()
+
+		loan_restructure = create_loan_restructure(
+			loan=loan.name,
+			restructure_date="2024-04-11",
+			interest_waiver_amount=500,
+			penal_waiver_amount=10,
+			other_charges_waiver=0,
+		)
+
+		loan_restructure.status = "Approved"
+		loan_restructure.save()
+
+		repayments = frappe.db.get_all(
+			"Loan Repayment",
+			filters={"loan_restructure": loan_restructure.name, "docstatus": 1},
+			pluck="name",
+		)
+
+		self.assertEqual(len(repayments), 6) # It will become 7 after the principal adjustment repayment is added (PR: #1060)
+
+	def test_unaccrued_interest_capitalization_gl_entries(self):
+		set_loan_accrual_frequency(loan_accrual_frequency="Daily")
+
+		loan = create_loan(
+			"_Test Customer 1",
+			"Term Loan Product 4",
+			100000,
+			"Repay Over Number of Periods",
+			22,
+			repayment_start_date="2024-04-05",
+			posting_date="2024-02-20",
+			rate_of_interest=8.5,
+			applicant_type="Customer",
+			penalty_charges_rate=36,
+		)
+
+		loan.submit()
+
+		make_loan_disbursement_entry(
+			loan.name, loan.loan_amount, disbursement_date="2024-02-20", repayment_start_date="2024-04-05"
+		)
+
+		process_loan_interest_accrual_for_loans(
+			posting_date="2024-04-04", loan=loan.name, company="_Test Company"
+		)
+
+		process_daily_loan_demands(loan=loan.name, posting_date="2024-04-05")
+
+		process_loan_interest_accrual_for_loans(loan=loan.name, posting_date="2024-04-10")
+
+		loan_restructure = create_loan_restructure(
+			loan=loan.name,
+			restructure_date="2024-04-11",
+			unaccrued_interest_waiver=100,
+		)
+
+		loan_restructure.status = "Approved"
+		loan_restructure.save()
+
+		loan_repayment = frappe.db.get_value(
+			"Loan Repayment",
+			{
+				"loan_restructure": loan_restructure.name,
+				"docstatus": 1,
+				"repayment_type": "Interest Capitalization",
+				"principal_amount_paid": (">", 0),
+			},
+			["name", "principal_amount_paid"],
+			as_dict=True,
+		)
+
+		amount = flt(loan_repayment.principal_amount_paid, 2)
+
+		gl_entries = frappe.db.get_all(
+			"GL Entry",
+			filters={
+				"voucher_type": "Loan Repayment",
+				"voucher_no": loan_repayment.name,
+				"is_cancelled": 0,
+			},
+			fields=["account", "debit", "credit"],
+		)
+
+		expected_entries = [
+			{"account": "Loan Account - _TC", "debit": amount, "credit": 0.0},
+			{"account": "Interest Receivable - _TC", "debit": 0.0, "credit": amount},
+		]
+
+		for expected in expected_entries:
+			self.assertIn(expected, gl_entries, f"Missing GL entry: {expected}")
+
+
+def create_loan_restructure(
+	loan,
+	restructure_date,
+	interest_waiver_amount=None,
+	unaccrued_interest_waiver=None,
+	penal_waiver_amount=None,
+	other_charges_waiver=None,
+	treatment_of_normal_interest="Capitalize",
+	unaccrued_interest_treatment="Capitalize",
+	treatment_of_penal_interest="Capitalize",
+	treatment_of_other_charges="Capitalize",
+):
+
+	doc = frappe.new_doc("Loan Restructure")
+	doc.loan = loan
+	doc.restructure_date = restructure_date
+	doc.interest_waiver_amount = interest_waiver_amount
+	doc.unaccrued_interest_waiver = unaccrued_interest_waiver
+	doc.penal_interest_waiver = penal_waiver_amount
+	doc.other_charges_waiver = other_charges_waiver
+	doc.restructure_type = "Normal Restructure"
+	doc.treatment_of_normal_interest = treatment_of_normal_interest
+	doc.unaccrued_interest_treatment = unaccrued_interest_treatment
+	doc.treatment_of_penal_interest = treatment_of_penal_interest
+	doc.treatment_of_other_charges = treatment_of_other_charges
+	doc.submit()
+
+	return doc
+>>>>>>> 7bf59d6f (fix: correct GL entries for unaccrued interest capitalization)
