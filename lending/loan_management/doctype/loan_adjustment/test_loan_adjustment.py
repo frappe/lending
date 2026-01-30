76a44e3326aca9aabfb922afdc8f6de13bfe68be
Nihantra C. Patel <141945075+Nihantra-Patel@users.noreply.github.com>
2026-01-30 12:14:17 +0530

    fix: resolved conflicts -v1

--- lending/loan_management/doctype/loan_adjustment/test_loan_adjustment.py
+++ lending/loan_management/doctype/loan_adjustment/test_loan_adjustment.py
@@ -2,13 +2,7 @@
 # See license.txt
 
 import frappe
-<<<<<<< HEAD
-
-# import frappe
 from frappe.tests.utils import FrappeTestCase
-=======
-from frappe.tests import IntegrationTestCase
->>>>>>> c7be395e (fix: Capitalization entries in loan restructure)
 from frappe.utils import getdate
 
 from lending.loan_management.doctype.process_loan_demand.process_loan_demand import (
