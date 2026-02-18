3a8f2c8a1bce554accd902bbce78ac0a3fcb3234
Nihantra Patel <nihantra@frappe.io>
2026-02-18 10:26:21 +0530

    fix: watch period handling for non-NPA restructure and later NPA transition
    (cherry picked from commit 6b6ee82d101dcad2eea0ab8a1420080eca9c2615)
    
    # Conflicts:
    #	lending/loan_management/doctype/loan/loan.py

--- lending/loan_management/doctype/loan/loan.py
+++ lending/loan_management/doctype/loan/loan.py
@@ -920,7 +920,7 @@ def update_days_past_due_in_loans(
 		applicant = loan_details.get("applicant")
 		company = loan_details.get("company")
 		freeze_date = loan_details.get("freeze_date")
-		watch_period_end_date = loan_details.get("watch_period_end_date")
+		existing_watch_period_end_date = loan_details.get("watch_period_end_date")
 
 		if not ignore_freeze and freeze_date and getdate(freeze_date) < getdate(posting_date):
 			return
@@ -943,6 +943,7 @@ def update_days_past_due_in_loans(
 				days_past_due = 0
 				is_npa = 0
 
+<<<<<<< HEAD
 			if watch_period_end_date and days_past_due == 1:
 				watch_period_days = frappe.db.get_value(
 					"Company", company, "watch_period_post_loan_restructure_in_days"
@@ -950,6 +951,8 @@ def update_days_past_due_in_loans(
 				watch_period_end_date = add_days(demand.demand_date, watch_period_days)
 				update_watch_period_date_for_all_loans(watch_period_end_date, applicant_type, applicant)
 
+=======
+>>>>>>> 6b6ee82d (fix: watch period handling for non-NPA restructure and later NPA transition)
 			if posting_date == add_days(getdate(), -1) or force_update_dpd_in_loan:
 				update_loan_and_customer_status(
 					demand.loan,
@@ -963,6 +966,24 @@ def update_days_past_due_in_loans(
 					loan_disbursement=disbursement,
 					is_backdated=is_backdated,
 					dpd_threshold=threshold,
+				)
+
+			watch_period_days = frappe.db.get_value(
+				"Company", company, "watch_period_post_loan_restructure_in_days"
+			)
+
+			watch_period_start_date = (
+				demand.demand_date
+				if existing_watch_period_end_date and days_past_due == 1
+				else getdate(posting_date)
+				if is_npa and not existing_watch_period_end_date
+				else None
+			)
+
+			if watch_period_start_date:
+				watch_period_end_date = add_days(watch_period_start_date, watch_period_days)
+				update_watch_period_date_for_all_loans(
+					watch_period_end_date, applicant_type, applicant
 				)
 
 			create_dpd_record(
