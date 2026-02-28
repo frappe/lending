4274b36bf295a05f3f9402d55b4ed844e0221d08
Deepesh Garg <deepeshgarg6@gmail.com>
2026-02-28 19:55:54 +0530

    fix: Future penal interest calculation
    (cherry picked from commit 1ca5fe77ee74d1550214c2f2c03e830f88070b43)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -2666,6 +2666,7 @@ def process_amount_for_loan(
 ):
 	from lending.loan_management.doctype.loan_interest_accrual.loan_interest_accrual import (
 		calculate_accrual_amount_for_loans,
+		calculate_penal_interest_for_loans,
 	)
 
 	precision = cint(frappe.db.get_default("currency_precision")) or 2
@@ -2713,6 +2714,16 @@ def process_amount_for_loan(
 		amounts["unaccrued_interest"] = calculate_accrual_amount_for_loans(
 			loan,
 			posting_date=(posting_date if payment_type == "Loan Closure" else add_days(posting_date, -1))
+			if not freeze_date
+			else freeze_date,
+			accrual_type="Regular",
+			is_future_accrual=1,
+			loan_disbursement=loan_disbursement,
+		)
+
+		amounts["unbooked_penalty"] = calculate_penal_interest_for_loans(
+			loan,
+			posting_date=add_days(posting_date, -1)
 			if not freeze_date
 			else freeze_date,
 			accrual_type="Regular",
