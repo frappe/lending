5734e791f6d48075789620e9ace1f59329e0c0f9
Deepesh Garg <deepeshgarg6@gmail.com>
2026-03-11 19:22:14 +0530

    chore: resolve conflicts

--- lending/loan_management/doctype/loan_product/loan_product.py
+++ lending/loan_management/doctype/loan_product/loan_product.py
@@ -125,7 +125,6 @@ class LoanProduct(Document):
 			if self.get(field) and self.get(field) < 0:
 				frappe.throw(_("{0} cannot be negative").format(frappe.unscrub(field)))
 
-<<<<<<< HEAD
 	def set_optional_accounts(self):
 		if not loan_accounting_enabled(self.company):
 			return
@@ -164,7 +163,6 @@ class LoanProduct(Document):
 				)
 			)
 
-=======
 	def validate_demand_offset_sequences(self):
 		mandatory_sequences = [
 			"collection_offset_sequence_for_standard_asset",
@@ -182,7 +180,6 @@ class LoanProduct(Document):
 						frappe.bold(frappe.unscrub(seq))
 					)
 				)
->>>>>>> a88657bb (fix: Loan Demand offset order non mandatory)
 
 @frappe.whitelist()
 def get_default_charge_accounts(charge_type, company):
