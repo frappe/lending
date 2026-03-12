8473426c2a574cffe80bd1f24d678dbd2fa553a7
Nihantra Patel <nihantra@frappe.io>
2026-03-12 17:25:04 +0530

    fix: correct interest calculation for Full Settlement and Write Off Settlement to exclude unbooked interest
    (cherry picked from commit 2e19d6068fd17393c7cfa5510a7b7233eed3134a)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -274,7 +274,7 @@ class LoanRepayment(AccountsController):
 			update_installment_counts(self.against_loan, loan_disbursement=self.loan_disbursement)
 
 		if self.repayment_type == "Full Settlement":
-			if not frappe.flags.in_test:
+			if frappe.flags.in_test:
 				job_name = frappe.enqueue(self.post_write_off_settlements, enqueue_after_commit=True)
 				self.db_set("full_settlement_job", job_name)
 			else:
