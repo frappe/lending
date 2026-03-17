f6a13131e2115ca207a02a353f10a6a70276482b
Nihantra Patel <nihantra@frappe.io>
2026-03-17 14:23:26 +0530

    fix: add Journal Entry to ignore list during Journal Entry cancellation
    (cherry picked from commit f0f36e1eeb957732b82320680c41ce89e20218d5)
    
    # Conflicts:
    #	lending/overrides/journal_entry.py

--- lending/overrides/journal_entry.py
+++ lending/overrides/journal_entry.py
@@ -1,6 +1,11 @@
 def add_ignore_linked_doctypes_for_jv(doc, method):
+<<<<<<< HEAD
 	existing = getattr(doc, "ignore_linked_doctypes", ())
 	lending_doctypes = ("Loan", "Loan Transfer", "Loan Interest Accrual")
+=======
+	existing = getattr(doc, 'ignore_linked_doctypes', ())
+	lending_doctypes = ("Loan", "Loan Transfer", "Loan Interest Accrual", "Journal Entry")
+>>>>>>> f0f36e1e (fix: add Journal Entry to ignore list during Journal Entry cancellation)
 	if existing:
 		doc.ignore_linked_doctypes = existing + lending_doctypes
 	else:
