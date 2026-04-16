d061a25c57a749df26cbc7eb5d4e2f18322d8bd5
Nihantra C. Patel <141945075+Nihantra-Patel@users.noreply.github.com>
2026-04-16 16:43:39 +0530

    fix: formatting

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -185,7 +185,11 @@ class LoanRepayment(AccountsController):
 			frappe.throw(_("Amount paid cannot be zero"))
 
 		if self.repayment_type == "Normal Repayment" and self.amount_paid > self.payable_amount:
-			frappe.throw(_("Amount paid ({0}) cannot be greater than overdue amount ({1})").format(flt(self.amount_paid), flt(self.payable_amount)))
+			frappe.throw(
+				_("Amount paid ({0}) cannot be greater than overdue amount ({1})").format(
+					flt(self.amount_paid), flt(self.payable_amount)
+				)
+			)
 
 	def book_unaccrued_interest(self):
 		precision = cint(frappe.db.get_default("currency_precision")) or 2
