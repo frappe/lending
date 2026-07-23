65f845b1762ab2b9d4fdffa017a6de1198c08bad
Deepesh Garg <deepeshgarg6@gmail.com>
2026-07-23 13:50:16 +0530

    fix: Reduce lock time
    (cherry picked from commit deec88044c9ee05c6a054d3c12c3376675aa2f45)
    (cherry picked from commit 7c979edf64e04ec84f9f578770568149c11adf11)

--- lending/loan_management/doctype/loan_repayment/loan_repayment.py
+++ lending/loan_management/doctype/loan_repayment/loan_repayment.py
@@ -153,16 +153,6 @@ class LoanRepayment(AccountsController):
 					)
 				)
 
-		amounts = calculate_amounts(
-			self.against_loan,
-			self.value_date,
-			payment_type=self.repayment_type,
-			charges=charges,
-			loan_disbursement=self.loan_disbursement,
-			for_update=True,
-		)
-
-		self.set_missing_values(amounts)
 		self.validate_repayment_type()
 		self.validate_disbursement_link()
 
@@ -174,9 +164,19 @@ class LoanRepayment(AccountsController):
 			self.validate_open_disbursement()
 		self.no_repayments_during_moratorium()
 		self.check_future_entries()
-		self.validate_security_deposit_amount()
-		self.validate_repayment_type()
 		self.set_partner_payment_ratio()
+
+		amounts = calculate_amounts(
+			self.against_loan,
+			self.value_date,
+			payment_type=self.repayment_type,
+			charges=charges,
+			loan_disbursement=self.loan_disbursement,
+			for_update=True,
+		)
+
+		self.set_missing_values(amounts)
+		self.validate_security_deposit_amount()
 		self.validate_amount(amounts)
 		self.allocate_amounts(amounts)
 
