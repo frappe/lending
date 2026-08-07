c71478b0cc1909a3fbddc627a519231cae65bc11
Nihantra Patel <nihantra@frappe.io>
2026-08-07 12:34:24 +0530

    refactor: rename doctype to Process Loan Accounting, drop gl_posted
    Address review comments on PR #1328:
    - rename Process Consolidated Loan GL doctype to Process Loan Accounting
      (shorter, less verbose), including the scheduler function and module
    - remove the gl_posted field on Loan Demand and Loan Interest Accrual.
      A doc past the consolidation cutoff with no consolidated_gl_voucher
      link is already "pending" -- no separate flag needed
    - change consolidated_gl_voucher from Data to Link
    - shorten the autoname to PLA-<month_end>-##### (loan is already
      visible on the form/list, no need to repeat it in the name)
    
    Fixes a bug the gl_posted removal uncovered: the pending-consolidation
    queries did not exclude docs dated before the company's start date, so
    a pre-cutoff doc (which already posted its own daily GL) could get
    wrongly folded into a consolidated voucher. Added a shared start-date
    guard used by get_deferred_docs, loan_has_deferred_gl and
    loans_with_deferred_gl.
    
    Added tests for edge cases that had no coverage before: co-lending
    demand GL posting the full amount (not partner_share), NPA suspense
    JEs for penal and additional interest (not just normal interest), and
    a repost whose repost_date is before the consolidation start date.
    
    (cherry picked from commit 228d6ad399ae846435017672f29b3054e13f05f0)
    
    # Conflicts:
    #	lending/hooks.py
    #	lending/loan_management/doctype/loan_demand/loan_demand.json
    #	lending/loan_management/doctype/loan_interest_accrual/loan_interest_accrual.json

--- lending/loan_management/doctype/process_loan_accounting/process_loan_accounting.js
+++ lending/loan_management/doctype/process_loan_accounting/process_loan_accounting.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Process Consolidated Loan GL", {
+frappe.ui.form.on("Process Loan Accounting", {
 	refresh(frm) {
 		if (frm.doc.docstatus > 0) {
 			frm.add_custom_button(
