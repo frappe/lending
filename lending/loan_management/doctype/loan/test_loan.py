09721ade942c4de5d2ef2896ad7ae49c76e1eb53
Nihantra Patel <nihantra@frappe.io>
2026-08-22 15:50:13 +0530

    test: rename regression test to a simpler, clearer name
    (cherry picked from commit c916bee663b708b844d48cc4614fa30c3c90828b)

--- lending/loan_management/doctype/loan/test_loan.py
+++ lending/loan_management/doctype/loan/test_loan.py
@@ -3649,7 +3649,7 @@ class TestLoan(FrappeTestCase):
 			frappe.db.set_single_value("Accounts Settings", "delete_linked_ledger_entries", 0)
 		self.assertFalse(frappe.db.exists("Loan", loan.name))
 
-	def test_partial_settlement_overshoot_creates_orphan_principal_demand(self):
+	def test_partial_settlement_extra_amount_not_added_to_principal(self):
 		loan = create_loan(
 			"_Test Customer 1",
 			"Term Loan Product 4",
